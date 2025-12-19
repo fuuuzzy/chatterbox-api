9fadf613dec50255accb07418ae90979569c58f5
g <ztingfg@foxmail.com>
2025-12-19 16:00:14 +0800

    fixed:update logic

--- chatterbox_tts.py
+++ chatterbox_tts.py
@@ -4,7 +4,7 @@ from chatterbox.tts import ChatterboxTTS
 
 if __name__ == '__main__':
     # Multilingual examples
-    model = ChatterboxTTS.from_pretrained(device="cuda", model_name="estevaofreitas/chatterbox-tts-ptbr")
+    model = ChatterboxTTS.from_pretrained(device="cuda", model_name="model")
 
     # multilingual_model = ChatterboxMultilingualTTS.from_pretrained(device="cuda",
     #                                                                model_name="estevaofreitas/chatterbox-tts-ptbr")
