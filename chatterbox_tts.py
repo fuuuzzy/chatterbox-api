41f3a3e0d4ad0eb9605ffbcaa30e9e67c906a2f4
g <ztingfg@foxmail.com>
2025-12-19 15:52:26 +0800

    fixed:update logic

--- chatterbox_tts.py
+++ chatterbox_tts.py
@@ -4,16 +4,18 @@ from chatterbox.tts import ChatterboxTTS
 
 if __name__ == '__main__':
     # Multilingual examples
-    multilingual_model = ChatterboxMultilingualTTS.from_pretrained(device="cuda",
-                                                                   model_name="estevaofreitas/chatterbox-tts-ptbr")
+    model = ChatterboxTTS.from_pretrained(device="cuda", model_name="estevaofreitas/chatterbox-tts-ptbr")
+
+    # multilingual_model = ChatterboxMultilingualTTS.from_pretrained(device="cuda",
+    #                                                                model_name="estevaofreitas/chatterbox-tts-ptbr")
 
     french_text = "que o verdadeiro herdeiro Sheng Da"
-    wav_french = multilingual_model.generate(french_text, language_id="pt-br")
-    ta.save("test-french.wav", wav_french, multilingual_model.sr)
+    wav_french = model.generate(french_text)
+    ta.save("test-french.wav", wav_french, model.sr)
 
     chinese_text = "你好，今天天气真不错，希望你有一个愉快的周末。"
-    wav_chinese = multilingual_model.generate(chinese_text, language_id="zh")
-    ta.save("test-chinese.wav", wav_chinese, multilingual_model.sr)
+    # wav_chinese = multilingual_model.generate(chinese_text, language_id="zh")
+    # ta.save("test-chinese.wav", wav_chinese, multilingual_model.sr)
     # # If you want to synthesize with a different voice, specify the audio prompt
     # AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
     # wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
